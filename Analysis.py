58a38e9189c9f1d48e496268d435e1ffec9e2057
Yuchen Xia <xiayuchen61@163.com>
2022-06-27 00:54:11 -0700

    changed Analysis

--- Analysis.py
+++ Analysis.py
@@ -1,4 +1,4 @@
-from math import cos, sin, acos, asin, degrees, radians, sqrt
+from math import cos, sin, acos, asin, degrees, radians, sqrt, atan2
 from Configuration import Mechanism
 
 class State:
@@ -9,10 +9,12 @@ class State:
 
     def analyze_points(self):
         m = self.mech
+        self.ax = 0
+        self.ay = 0
         self.ex = self.fx - m.lef * cos(radians(m.beta3))
         self.ey = self.fy - m.lef * sin(radians(m.beta3))
         self.dx = self.ex - m.lde
-        self.dy = self.ey 
+        self.dy = self.ey
         self.cx = self.dx - m.lcd
         self.cy = self.dy
         self.alpha1 = self.calculate_alpha1(m)
@@ -21,6 +23,14 @@ class State:
         self.by = m.lab * sin(radians(self.alpha1))
         self.gx = m.lag * cos(radians(self.alpha2))
         self.gy = m.lag * cos(radians(self.alpha2))
+        self.hx = m.lah * cos(radians(self.alpha2 - m.beta1))
+        self.hy = m.lah * sin(radians(self.alpha2 - m.beta1))
+        self.mx = self.hx + m.lan
+        self.my = self.hy
+        self.nx = self.ax + m.lan
+        self.ny = self.ay
+        self.kx = self.gx + m.lan
+        self.ky = self.gy
 
     def analyze_pressure_angles(self):
         pass
@@ -62,6 +72,9 @@ class State:
             return
         return alpha2_cand[0]
 
+    def calculate_angle_abc(self, a, b, c):
+        ang = degrees(atan2(c[1]-b[1], c[0]-b[0]) - atan2(a[1]-b[1], a[0]-b[0]))
+        return abs(ang)
 
 #
 # Helper Functions for caculating angles
