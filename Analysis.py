0c1aa1a99baf7d41c54e5c08514b5ccd0a8e30e6
Yuchen Xia <xiayuchen61@163.com>
2022-06-27 23:10:29 -0700

    Completed Analysis.py

--- Analysis.py
+++ Analysis.py
@@ -4,47 +4,39 @@ from Configuration import Mechanism
 class State:
     def __init__(self, mechanism:Mechanism, x, y) -> None:
         self.mech = mechanism
-        self.fx = x
-        self.fy = y
+        self.f = (x, y)
 
-    def analyze_points(self):
+    def analyze(self):
+        # Points
         m = self.mech
-        self.ax = 0
-        self.ay = 0
-        self.ex = self.fx - m.lef * cos(radians(m.beta3))
-        self.ey = self.fy - m.lef * sin(radians(m.beta3))
-        self.dx = self.ex - m.lde
-        self.dy = self.ey
-        self.cx = self.dx - m.lcd
-        self.cy = self.dy
+        self.a = (0, 0)
+        self.e = (self.f[0] - m.lef * cos(radians(m.beta3)), self.f[1] - m.lef * sin(radians(m.beta3)))
+        self.d = (self.e[0] - m.lde, self.e[1])
+        self.c = (self.d[0] - m.lcd, self.d[1])
         self.alpha1 = self.calculate_alpha1(m)
         self.alpha2 = self.calculate_alpha2(m)
-        self.bx = m.lab * cos(radians(self.alpha1))
-        self.by = m.lab * sin(radians(self.alpha1))
-        self.gx = m.lag * cos(radians(self.alpha2))
-        self.gy = m.lag * cos(radians(self.alpha2))
-        self.hx = m.lah * cos(radians(self.alpha2 - m.beta1))
-        self.hy = m.lah * sin(radians(self.alpha2 - m.beta1))
-        self.mx = self.hx + m.lan
-        self.my = self.hy
-        self.nx = self.ax + m.lan
-        self.ny = self.ay
-        self.kx = self.gx + m.lan
-        self.ky = self.gy
-
-    def analyze_pressure_angles(self):
-        pass
+        self.b = (m.lab * cos(radians(self.alpha1)), m.lab * sin(radians(self.alpha1)))
+        self.g = (m.lag * cos(radians(self.alpha2)), m.lag * cos(radians(self.alpha2)))
+        self.h = (m.lah * cos(radians(self.alpha2 - m.beta1)), m.lah * sin(radians(self.alpha2 - m.beta1)))
+        self.m = (self.h[0] + m.lan, self.h[1])
+        self.n = (self.a[0] + m.lan, self.a[1])
+        self.k = (self.g[0] + m.lan, self.g[1])
+        # Pressure Angles
+        self.abc = calculate_angle(self.a, self.b, self.c)
+        self.cdg = calculate_angle(self.c, self.c, self.g)
+        self.agd = calculate_angle(self.a, self.g, self.d)
+        self.ahm = calculate_angle(self.a, self.h, self.m)
         
     def calculate_alpha1(self, m:Mechanism):
         # Equation here based on lbc
-        rhs_bc = (m.lab**2 - m.lbc**2 + self.cx**2 + self.cy**2)/(2*m.lab)
-        alpha1_cand = solve_alpha(self.cx, self.cy, rhs_bc)
+        rhs_bc = (m.lab**2 - m.lbc**2 + self.c[0]**2 + self.c[1]**2)/(2*m.lab)
+        alpha1_cand = solve_alpha(self.c[0], self.c[1], rhs_bc)
         if alpha1_cand is None:
             print('Error: No Possible Resolution')
             return
         for value in alpha1_cand:
             bx = m * cos(radians(value))
-            if bx >= self.cx:
+            if bx >= self.c[0]:
                 alpha1_cand.remove(value)
         if len(alpha1_cand) == 0:
             print('Error: No valid alpha1 value')
@@ -56,8 +48,8 @@ class State:
 
     def calculate_alpha2(self, m:Mechanism):
         # Equation here based on ldg
-        rhs_dg = (m.lag**2 - m.ldg**2 + self.dx**2 + self.dy**2)/(2*m.lag)
-        alpha2_cand = solve_alpha(self.dx, self.dy, rhs_dg)
+        rhs_dg = (m.lag**2 - m.ldg**2 + self.d[0]**2 + self.d[1]**2)/(2*m.lag)
+        alpha2_cand = solve_alpha(self.d[0], self.d[1], rhs_dg)
         if alpha2_cand is None:
             print('Error: No Possible Resolution')
             return
@@ -71,10 +63,6 @@ class State:
             print('Error: Fail to obtain sole solution for alpha2')
             return
         return alpha2_cand[0]
-
-    def calculate_angle_abc(self, a, b, c):
-        ang = degrees(atan2(c[1]-b[1], c[0]-b[0]) - atan2(a[1]-b[1], a[0]-b[0]))
-        return abs(ang)
 
 #
 # Helper Functions for caculating angles
@@ -112,3 +100,8 @@ def solve_asum(sin_v):
         asum.append(180-angle)
         asum.append(360+angle)
     return asum
+
+# Calculate the angle given three points
+def calculate_angle(a, b, c):
+        ang = degrees(atan2(c[1]-b[1], c[0]-b[0]) - atan2(a[1]-b[1], a[0]-b[0]))
+        return abs(ang)
