c685134a1a119bcd1909787a693dab4baa24c0dd
cjy <bennycjy@Benny-MacBook-Pro-2.local>
2022-07-07 17:08:08 -0400

    modify the trajectory formula

--- main.py
+++ main.py
@@ -20,42 +20,39 @@ beta3 = float(input("输入EF与水平夹角（角度制）"))
 print("轨迹方程选项，角度从小到大填")
 print("1. 3-4-5")
 print("2. 4-5-6-7")
-interval_lst = []
 formula_lst = []
 h_lst = []
 beta_lst = []
-j = 0
-while j != 360:
-    i = float(input("输入左区间"))
-    j = float(input("输入右区间"))
-    if i > j:
-        print("错误区间")
-        continue
+while sum(beta_lst) != 360:
     formula = int(input("选择轨迹方程"))
+    if formula not in [1, 2]:
+        print("错误方程选项")
+        continue
+    beta = float(input("输入转动角度beta1"))
     h = float(input("输入参数h"))
-    beta = float(input("输入参数beta1"))
     h_lst.append(h)
     beta_lst.append(beta)
-    interval_lst.append((i, j))
     formula_lst.append(formula)
 
 
 # acquire the list of enpoints tuple
-def get_endpoints(interval_lst, formula_lst, h_lst, beta_lst):
+def get_endpoints(formula_lst, h_lst, beta_lst):
     endpoint_lst = []
-    for index in range(0, len(interval_lst)):
-        for theta in [round(num,1) for num in np.arange(interval_lst[index][0], interval_lst[index][1] + 0.5, 0.5)]:
+    for index in range(0, len(beta_lst)):
+        for theta in [round(num,1) for num in np.arange(0, beta_lst[index] + 0.5, 0.5)]:
             if formula_lst[index] == 1:
-                s = h_lst[index] * (10 * pow(theta/beta_lst[index], 3) - 15 * pow(theta/beta_lst[index], 4) + 6 * pow(theta/beta_lst[index], 5))
+                s = sum(h_lst[0: index + 1]) * (10 * pow(theta/beta_lst[index], 3) - 15 * pow(theta/beta_lst[index], 4) + 6 * pow(theta/beta_lst[index], 5))
             else:
-                s = h_lst[index] * (35 * pow(theta/beta_lst[index], 4) - 84 * pow(theta/beta_lst[index], 5) + 70 * pow(theta/beta_lst[index], 6) - 20 * pow(theta/beta_lst[index], 7))
+                s = sum(h_lst[0: index + 1]) * (35 * pow(theta/beta_lst[index], 4) - 84 * pow(theta/beta_lst[index], 5) + 70 * pow(theta/beta_lst[index], 6) - 20 * pow(theta/beta_lst[index], 7))
             endpoint_lst.append((s * math.cos(theta), s * math.sin(theta)))
     return endpoint_lst
 
 # prepare for analysis
 mech = Mechanism(lab, lbc, lcd, lde, lhm, lan, lef, ldg, lek, lag, lnk, lah, lnm, beta1, beta2, beta3)
-endpoints = get_endpoints(interval_lst, formula_lst, h_lst, beta_lst)
+endpoints = get_endpoints(formula_lst, h_lst, beta_lst)
 
 state_lst = []
 for endpoint in endpoints:
-    state = State(mech, endpoint[0], endpoint[1])
+        state = State(mech, endpoint[0], endpoint[1])
+        state.analyze()
+        state_lst.append(state)
